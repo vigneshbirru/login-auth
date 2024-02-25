e9a8875ff39c7b792d805b8557cf59719d051ac8
Birru vighnesh <78299713+vigneshbirru@users.noreply.github.com>
2024-02-25 13:31:14 +0000

    database connected

--- database/database.js
+++ database/database.js
@@ -1,16 +1,16 @@
-const mongoose =require("mongoose")
-const MONGODB_URL = process.env
+const mongoose = require("mongoose");
+const { MONGODB_URL } = process.env;
 
-
-exports.connect = ()=>{
-    mongoose.connect(MONGODB_URL,{
-        userNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
-    .then()
-    .catch((e)=>{
-        console.log("Database connection failed")
-        console.log(e)
-        process.exit(1)
-    })
-}
+exports.connect = async () => {
+    try {
+        console.log(typeof MONGODB_URL);
+        await mongoose.connect(MONGODB_URL, {  
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("Database connected successfully");
+    } catch (error) {
+        console.error("Database connection failed:", error);
+        process.exit(1);
+    }
+};
